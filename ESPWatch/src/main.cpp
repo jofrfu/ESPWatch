58a92eb9f949ec169a8cbee42f55f2e4942e7316
Jonas Fuhrmann <jofrfu@googlemail.com>
2018-07-11 19:14:37 +0200

    lambda usage

--- ESPWatch/src/main.cpp
+++ ESPWatch/src/main.cpp
@@ -10,16 +10,12 @@
 #include "BLEHandler.h"
 #include <freertos/timers.h>
 
-void timerCallback(TimerHandle_t xTimer);
-
-ScreenQueue *screenQueue;
-
 void setup() {
     Serial.begin(115200);
     delay(5000);
 
     auto screen = new Screen();
-    screenQueue = new ScreenQueue(screen);
+    auto screenQueue = new ScreenQueue(screen);
     auto decoder = new Decoder();
     auto dispatcher = new Dispatcher(decoder, screenQueue);
     auto bleHandler = new BLEHandler(dispatcher);
@@ -30,18 +26,18 @@ void setup() {
         Serial.println("Error creating thread screenThread.");
     }
 
-    TimerHandle_t timer = xTimerCreate("ClockTimer", pdMS_TO_TICKS(60000), pdTRUE, (void*)0, timerCallback);
+    TimerCallbackFunction_t callback = [screenQueue](TimerHandle_t xTimer){
+        ScreenQueue::parameter_t parameter;
+        parameter.type = ScreenQueue::UPDATE;
+        screenQueue->addFunctionCall(parameter);
+    };
+
+    TimerHandle_t timer = xTimerCreate("ClockTimer", pdMS_TO_TICKS(60000), pdTRUE, (void*)0, callback);
     if(xTimerStart(timer, 10) != pdPASS) {
         Serial.println("Error starting timer.");
     }
 
     pthread_join(screenThread, nullptr);
-}
-
-void timerCallback(TimerHandle_t xTimer) {
-    ScreenQueue::parameter_t parameter;
-    parameter.type = ScreenQueue::UPDATE;
-    screenQueue->addFunctionCall(parameter);
 }
 
 void loop() {}
